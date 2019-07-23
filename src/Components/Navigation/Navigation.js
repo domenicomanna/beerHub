5bdafdda3c9e4056385a039ff838adcda8529e29
Domenico Manna <mannadomenico2849@gmail.com>
2019-07-22 20:43:41 -0400

    Adjust styling

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -3,17 +3,19 @@ import styles from './Navigation.module.css';
 import Wrapper from '../Wrapper/Wrapper';
 const Navigation = () => {
     return (
-        <Wrapper>
-            <header className={styles.header}>
-                <h2 className={styles.company}> BeerHub</h2>
-                <nav>
-                    <ul className={styles.navigation}>
-                        <li> <a className={styles.active}> Home </a> </li>
-                        <li> <a className={styles.active}> Home </a> </li>
-                    </ul>
-                </nav>
-            </header>
-        </Wrapper>
+        <div className={styles.navigation}>
+            <Wrapper>
+                <header className={styles.header}>
+                    <h2 className={styles.company}> BeerHub</h2>
+                    <nav>
+                        <ul className={styles.navigationList}>
+                            <li> <a className={styles.active}> Home </a> </li>
+                            <li> <a className={styles.active}> Home </a> </li>
+                        </ul>
+                    </nav>
+                </header>
+            </Wrapper>
+        </div>
     );
 }
 
