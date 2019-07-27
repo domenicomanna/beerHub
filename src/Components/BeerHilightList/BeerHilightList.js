b63c166ddfa2bb59619668ab28a8e4d63df978f4
Domenico Manna <mannadomenico2849@gmail.com>
2019-07-27 11:58:58 -0400

    Shorten beer description for every beer

--- src/Components/BeerHilightList/BeerHilightList.js
+++ src/Components/BeerHilightList/BeerHilightList.js
@@ -19,9 +19,20 @@ function transformBeers(beers) {
         <BeerHilight
             key={beer.id}
             name={beer.name}
-            description={beer.description}
+            description={shortenLength(beer.description, 25)}
             image={beer.image_url} />
     ));
 
+}
+
+
+function shortenLength(string, maxLength){
+    let shortenedString = string.split(" ");
+    shortenedString.length = shortenedString.length < maxLength ? shortenedString.length - 1 : maxLength;
+
+    // if the last word in the array is a puncutation mark, remove it
+    if (/[.,?/-]/.test(shortenedString[shortenedString.length - 1])) shortenedString.pop();
+    
+    return shortenedString.join(" ") + "...";
 }
 export default BeerHilightList;
