49e2805963c0ce57306833584705f999553e0d07
Domenico Manna <mannadomenico2849@gmail.com>
2019-08-03 14:02:12 -0400

    Add method to return all favorited beers in an array

--- src/beerStorage.js
+++ src/beerStorage.js
@@ -13,11 +13,10 @@ export default class BeerStorage{
     // Returns a map of the all the favorited beers. The key is the beer id
     // and the value is the beer. If no beers are favorited, an empty map will be returned
     getFavorites(){
-        let beers = window.localStorage.getItem(BEER_KEY);
-        if (!beers) return new Map();
-        
-        beers = JSON.parse(beers);
+        let beers = this.getFavoritesAsArray();
         let mappedBeers = new Map();
+
+        if (beers.length === 0) return mappedBeers;
 
         beers.forEach(beer => {
             mappedBeers.set(beer.id, beer);
@@ -25,5 +24,13 @@ export default class BeerStorage{
 
         return mappedBeers;
         
+    }
+
+    // Returns all the favorited beers in an array
+    getFavoritesAsArray(){
+        let beers = window.localStorage.getItem(BEER_KEY);
+        if (!beers) return [];
+        
+        return JSON.parse(beers);
     }
 }
