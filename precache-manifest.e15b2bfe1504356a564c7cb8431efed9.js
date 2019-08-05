b883d91f34a6aa712fda190cc3d8388936bd9ad0
Domenico Manna <mannadomenico2849@gmail.com>
2019-08-05 18:29:14 -0400

    Updates

---
+++ precache-manifest.e15b2bfe1504356a564c7cb8431efed9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aadceeabd64e6a04be05daf3ebcd555d",
+    "url": "/domenicomanna/beerHub/index.html"
+  },
+  {
+    "revision": "f086f13e4c6993044014",
+    "url": "/domenicomanna/beerHub/static/css/2.6dadf8a3.chunk.css"
+  },
+  {
+    "revision": "abeca51292b023191810",
+    "url": "/domenicomanna/beerHub/static/css/main.362ee2fd.chunk.css"
+  },
+  {
+    "revision": "f086f13e4c6993044014",
+    "url": "/domenicomanna/beerHub/static/js/2.9922896f.chunk.js"
+  },
+  {
+    "revision": "abeca51292b023191810",
+    "url": "/domenicomanna/beerHub/static/js/main.3fdc34e4.chunk.js"
+  },
+  {
+    "revision": "dc7a84e04a9526d9e81b",
+    "url": "/domenicomanna/beerHub/static/js/runtime~main.8041eb64.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/domenicomanna/beerHub/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/domenicomanna/beerHub/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/domenicomanna/beerHub/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/domenicomanna/beerHub/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/domenicomanna/beerHub/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
